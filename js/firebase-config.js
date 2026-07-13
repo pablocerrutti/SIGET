5bd32fba6cc3558449fc9896916c9827cb18be6d
pablocerrutti <pablocerrutti@gmail.com>
2026-07-13 12:24:08 -0300

    Update firebase-config.js

--- js/firebase-config.js
+++ js/firebase-config.js
@@ -9,16 +9,25 @@ import { getDatabase } from
 
 
 const firebaseConfig = {
+
     apiKey: "AIzaSyAIMMzBpN1I2DcyaXX-a01vVfWiIjyZKMc",
+
     authDomain: "siget-f6b52.firebaseapp.com",
+
     databaseURL: "https://siget-f6b52-default-rtdb.firebaseio.com",
+
     projectId: "siget-f6b52",
+
     storageBucket: "siget-f6b52.firebasestorage.app",
+
     messagingSenderId: "846129054307",
+
     appId: "1:846129054307:web:9c23f714681f28c3026e56"
+
 };
 
 
 const app = initializeApp(firebaseConfig);
+
 
 export const db = getDatabase(app);
